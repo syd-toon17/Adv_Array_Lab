318a47521a0788516d598806c659ac25d3ec4dea
Sydney Wilson <syd.toon17@gmail.com>
2022-02-28 11:15:43 -0600

    completed problems 1 to 3.

--- app.js
+++ app.js
@@ -126,19 +126,45 @@ console.log('Mexican Foods: ', mexicanFood)
 //1. Create a function that will return all dishes with the cuisine type of "vegetarian"
 //Filter
 
-function problemOne(){
+function findVegetarianFood(){
 
-    let results;
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Vegetarian"){
+
+            return true;
+        }
+        else{
+            return false;
+        }})
 
 
     return results;
 }
 
+let vegetarianFood = findVegetarianFood();
+console.log("Vegetarian Foods: ", vegetarianFood)
+
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
 
+function findItalianFood(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Italian" && el.servings > 5){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+let italianFood = findItalianFood();
+console.log("Big Serving Italian Foods: ", italianFood)
+
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
+
+
 
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
